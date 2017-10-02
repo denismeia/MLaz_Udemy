d5a35148bc6cab655ce934421895258bc231c6bb
Denis Mariano <denismariano@deniss-air.unl.edu>
2017-10-02 14:12:18 -0500

    updated udemy

--- Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
+++ Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)/upper_confidence_bound.py
@@ -1,14 +1,14 @@
 # Upper Confidence Bound
 
-# Importing the libraries
+#%% Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-# Importing the dataset
+import os
+os.chdir('/Users/denismariano/pcloud/LEARNING/Machine Learning A-Z Template Folder/Part 6 - Reinforcement Learning/Section 32 - Upper Confidence Bound (UCB)')
 dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
-# Implementing UCB
+#%% Implementing UCB
 import math
 N = 10000
 d = 10
@@ -35,7 +35,7 @@ for n in range(0, N):
     sums_of_rewards[ad] = sums_of_rewards[ad] + reward
     total_reward = total_reward + reward
 
-# Visualising the results
+#%% Visualising the results
 plt.hist(ads_selected)
 plt.title('Histogram of ads selections')
 plt.xlabel('Ads')
